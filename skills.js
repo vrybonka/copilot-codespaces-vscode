cbcb672a8e4b477ac6f209dc9d0079f202427a8d
Viacheslav <60274572+vrybonka@users.noreply.github.com>
2026-02-25 17:47:07 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,15 +1,3 @@
 function CalculateNumbers (var1, var2) {
-    var sum = var1 + var2;
-    var difference = var1 - var2;
-    var product = var1 * var2;
-    var quotient = var1 / var2;
-
-    return {
-        sum: sum,
-        difference: difference,
-        product: product,
-        quotient: quotient
-    };
+    console.log(var1 + var2);
 }
-
-module.exports = CalculateNumbers;      
